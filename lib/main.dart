c528ceed963f65af2c41da7fdb38e92a17e7084e
chellarobiatul1 <chellarobiatul1@gmail.com>
2025-09-10 10:29:42 +0700

    menghilangkan debug show

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Aplikasi Film',
       theme: ThemeData(
     scaffoldBackgroundColor: Color(0xFFFFF1F3), // pastel pink background
